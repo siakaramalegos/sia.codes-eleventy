aea467cd66e527756abf7244971f0f10dbe9aec4
Sia Karamalegos <siakaramalegos@gmail.com>
2020-12-15 15:25:39 -0600

    Add Twitter image variation

--- src/_11ty/shortcodes.js
+++ src/_11ty/shortcodes.js
@@ -6,17 +6,18 @@ const FALLBACK_WIDTH = 680;
 const SEO_ASPECT_RATIOS = ["1:1", "4:3", "16:9"]
 
 // Social share image configuration
-const SHARE_FILE = "share_tmpl.jpg"
+const SHARE_IMAGE_FILE = "share_tmpl.jpg"
+const TWITTER_IMAGE_FILE = "twitter_tmpl.jpg"
 // If font not in the root of your Cloudinary media library, need to prepend with `foldername:`
 const TITLE_FONT = "RecursiveSansExtraBold.woff2"
 const TITLE_FONT_SIZE = "60"
-const TITLE_BOTTOM_OFFSET = "276"
+const TITLE_BOTTOM_OFFSET = 282
 const TAGLINE_FONT = "RecursiveSansRegular.woff2"
 const TAGLINE_FONT_SIZE = "36"
-const TAGLINE_TOP_OFFSET = "387"
+const TAGLINE_TOP_OFFSET = 380
 const TAGLINE_LINE_HEIGHT = "10"
 const TEXT_AREA_WIDTH = "705"
-const TEXT_LEFT_OFFSET = "425"
+const TEXT_LEFT_OFFSET = 425
 const TEXT_COLOR = "221f2c"
 
 function getSrcset(file, widths) {
@@ -39,10 +40,17 @@ function cloudinarySafeText(text) {
   return encodeURIComponent(text).replace(/(%2C)/g, '%252C').replace(/(%2F)/g, '%252F')
 }
 
-function socialImageUrl(title, description) {
+function socialImageUrl(title, description, isTwitter = false) {
+  const file = isTwitter ? TWITTER_IMAGE_FILE : SHARE_IMAGE_FILE
+  const width = isTwitter ? "1280" : "1200"
+  const height = isTwitter ? "640" : "630"
+  const left_offset = isTwitter ? TEXT_LEFT_OFFSET + 30 : TEXT_LEFT_OFFSET
+  const topOffset = isTwitter ? TAGLINE_TOP_OFFSET - 24 : TAGLINE_TOP_OFFSET
+  const bottomOffset = isTwitter ? TITLE_BOTTOM_OFFSET + 24 : TITLE_BOTTOM_OFFSET
+
   const imageConfig = [
-    "w_1200",
-    "h_630",
+    `w_${width}`,
+    `h_${height}`,
     "q_auto:best",
     "c_fill",
     "f_jpg",
@@ -53,8 +61,8 @@ function socialImageUrl(title, description) {
     'c_fit',
     `co_rgb:${TEXT_COLOR}`,
     'g_south_west',
-    `x_${TEXT_LEFT_OFFSET}`,
-    `y_${TITLE_BOTTOM_OFFSET}`,
+    `x_${left_offset}`,
+    `y_${bottomOffset}`,
     `l_text:${TITLE_FONT}_${TITLE_FONT_SIZE}:${cloudinarySafeText(title)}`,
   ].join(",")
 
@@ -63,12 +71,12 @@ function socialImageUrl(title, description) {
     'c_fit',
     `co_rgb:${TEXT_COLOR}`,
     'g_north_west',
-    `x_${TEXT_LEFT_OFFSET}`,
-    `y_${TAGLINE_TOP_OFFSET}`,
+    `x_${left_offset}`,
+    `y_${topOffset}`,
     `l_text:${TAGLINE_FONT}_${TAGLINE_FONT_SIZE}_line_spacing_${TAGLINE_LINE_HEIGHT}:${cloudinarySafeText(description)}`,
   ].join(',');
 
-  return `${BASE_URL}${imageConfig}/${titleConfig}/${taglineConfig}/${FOLDER}${SHARE_FILE}`
+  return `${BASE_URL}${imageConfig}/${titleConfig}/${taglineConfig}/${FOLDER}${file}`
 }
 
 module.exports = {
@@ -78,5 +86,5 @@ module.exports = {
     const imageSet = SEO_ASPECT_RATIOS.map(aspectRatio => fullSizeCrop(file, aspectRatio))
     return JSON.stringify(imageSet)
   },
-  socialImage: (title, description) => socialImageUrl(title, description),
+  socialImage: (title, description, isTwitter) => socialImageUrl(title, description, isTwitter),
 }
