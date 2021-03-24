aff62a14ba3e295c6a8df7708422df31760dcd87
Sia Karamalegos <siakaramalegos@gmail.com>
2021-03-24 13:40:52 -0500

    Add budget check for lighthouse ci

--- lighthouserc.js
+++ lighthouserc.js
@@ -5,12 +5,21 @@ module.exports = {
       // staticDistDir: './_site',
       url: [
         'http://localhost:8081/',
-        'http://localhost:8081/posts/making-google-fonts-faster/',
-        'http://localhost:8081/posts/responsive-images-perf-matters-video/'
-      ]
+        // 'http://localhost:8081/posts/making-google-fonts-faster/',
+        // 'http://localhost:8081/posts/responsive-images-perf-matters-video/'
+      ],
+      numberOfRuns: 1,
     },
     upload: {
       target: 'temporary-public-storage',
     },
+    assert: {
+      budgetsFile: "./budget.json",
+      assertions: {
+        'categories:performance': ['error', {minScore: 95}],
+        'categories:accessibility': ['error', {minScore: 95}]
+      }
+
+    }
   },
 };
