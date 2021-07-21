d6445795fc2fe8071d7e18d341e2926dd6e0db94
Sia Karamalegos <siakaramalegos@gmail.com>
2021-07-21 09:31:57 -0500

    Stop running twitter embed page

--- lighthouserc.js
+++ lighthouserc.js
@@ -4,10 +4,10 @@ module.exports = {
       staticDistDir: "./_site",
       url: [
         "http://localhost:8081/",
-        "http://localhost:8081/posts/making-google-fonts-faster/",
+        // "http://localhost:8081/posts/making-google-fonts-faster/",
         "http://localhost:8081/posts/responsive-images-perf-matters-video/",
       ],
-      numberOfRuns: 3,
+      numberOfRuns: 1,
     },
     upload: {
       target: "temporary-public-storage",
