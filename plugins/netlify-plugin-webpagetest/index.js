c3ce37c7d38bb3e89308a813f848cb071695a973
Sia Karamalegos <siakaramalegos@gmail.com>
2021-07-25 15:07:08 -0500

    Add WPT API request

--- plugins/netlify-plugin-webpagetest/index.js
+++ plugins/netlify-plugin-webpagetest/index.js
@@ -1,8 +1,29 @@
+const fetch = require('node-fetch');
+const BASE_URL = 'https://www.webpagetest.org/runtest.php'
+
 module.exports = {
-  onPreBuild: ({netlifyConfig}) => {
-    console.log('Hello world from onPreBuild event!')
-    console.log('token', process.env.WEBPAGETEST_API_TOKEN);
-    console.log('netlifyConfig.build.environment', netlifyConfig.build.environment);
-    console.log('url', netlifyConfig.build.environment.DEPLOY_PRIME_URL);
+  onSuccess: async ({netlifyConfig}) => {
+    const deployUrl = netlifyConfig.build.environment.DEPLOY_PRIME_URL
+
+    if (!deployUrl) {
+      console.log('Skipping WebPageTest: no deploy URL')
+      return
+    }
+
+
+    try {
+      console.log('Beginning WebPageTest run...')
+      const result = await fetch(`${BASE_URL}?url=${deployUrl}&k=${process.env.WEBPAGETEST_API_TOKEN}&f=json`)
+
+      if (result.ok) {
+        const json = await result.json()
+        console.log('Success! Url is ', json.data.userUrl);
+      } else {
+        return failPlugin(`WebPageTest request failed with error ${result.status}: ${result.statusText}`)
+      }
+    } catch (error) {
+      return failPlugin(`WebPageTest plugin failed with error: ${error}`)
+    }
+
   },
 }
