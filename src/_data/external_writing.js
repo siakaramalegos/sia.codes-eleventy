10f1cfe593ba2e733f5a1e2783994cb05da27a6b
Sia Karamalegos <siakaramalegos@gmail.com>
2021-12-06 08:21:23 -0500

    add web almanac chapter to external writing

--- src/_data/external_writing.js
+++ src/_data/external_writing.js
@@ -1,5 +1,13 @@
 module.exports = [
   {
+    title: "Web Almanac 2021: Performance",
+    description: "The state of web performance in 2021 including the Core Web Vitals (LCP, CLS, FID) as well as FCP and TTFB for the top ~8M websites.",
+    url: "https://almanac.httparchive.org/en/2021/performance",
+    date: new Date('2021-12-2'),
+    publicationName: "Web Almanac by HTTP Archive",
+    tags: ['WebPerf', 'External']
+  },
+  {
     title: "8 Things to Know About React for Drupal Devs",
     description: "Get a taste of why developers are so happy with React and learn about how some of the concepts are similar or different from the Drupal and PHP world",
     url: "https://reactfordrupal.com/tutorials/react-for-drupal-developers/",
