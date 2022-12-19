6fa137c9a615b92ba253e4f95f0fed6e222ba483
Sia Karamalegos <siakaramalegos@gmail.com>
2022-12-19 13:37:38 -0500

    Add Shopify articles

--- src/_data/external_writing.js
+++ src/_data/external_writing.js
@@ -1,4 +1,52 @@
 module.exports = [
+  // {
+  //   title: "",
+  //   description: "",
+  //   url: "",
+  //   date: new Date('2022-12-2'),
+  //   publicationName: "",
+  //   tags: ['WebPerf', 'External']
+  // },
+  // {
+  //   title: "",
+  //   description: "",
+  //   url: "",
+  //   date: new Date('2022-12-2'),
+  //   publicationName: "Performance at Shopify",
+  //   tags: ['WebPerf', 'External']
+  // },
+  {
+    title: "Optimizing images for performance on Shopify",
+    description: "Images are key to user experience, especially in ecommerce. Making images smaller doesn't always result in better performance. In this post, I focus on how images can impact page load speed and layout shift.",
+    url: "https://performance.shopify.com/blogs/blog/optimizing-images-for-performance-on-shopify",
+    date: new Date('2022-11-18'),
+    publicationName: "Performance at Shopify",
+    tags: ['WebPerf', 'Images', 'External']
+  },
+  {
+    title: "Responsive images on Shopify with Liquid",
+    description: "Responsive images are important for both user experience and site speed. Get them right, and your users will be more likely to convert. Get them wrong, and you may cause your site to be slow.",
+    url: "https://performance.shopify.com/blogs/blog/responsive-images-on-shopify-with-liquid",
+    date: new Date('2022-10-11'),
+    publicationName: "Performance at Shopify",
+    tags: ['WebPerf', 'Images', 'External']
+  },
+  {
+    title: "Lazy load images for performance",
+    description: "Lazy loading is a performance strategy that de-prioritizes assets like images that are not needed immediately after a webpage is loaded. As with any strategy, you can misuse it causing worse performance.",
+    url: "https://performance.shopify.com/blogs/blog/lazy-load-images-for-performance",
+    date: new Date('2022-08-08'),
+    publicationName: "Performance at Shopify",
+    tags: ['WebPerf', 'Images', 'External']
+  },
+  {
+    title: "Fix web performance for the long-term",
+    description: "At Shopify, we’ve seen what works and what doesn’t work when companies want to improve their web performance. We want to share with you our top learnings to get you on the path of success.",
+    url: "https://performance.shopify.com/blogs/blog/how-to-achieve-and-maintain-web-performance",
+    date: new Date('2022-7-25'),
+    publicationName: "Performance at Shopify",
+    tags: ['WebPerf', 'External']
+  },
   {
     title: "Web Almanac 2021: Performance",
     description: "The state of web performance in 2021 including the Core Web Vitals (LCP, CLS, FID) as well as FCP and TTFB for the top ~8M websites.",
