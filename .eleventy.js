888c53fbd90052a43ed021dc0fc26977679546c3
Sia <siakaramalegos@gmail.com>
2023-08-08 09:11:33 -0400

    Update .eleventy.js to pass through robots.txt

--- .eleventy.js
+++ .eleventy.js
@@ -36,6 +36,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/img");
   eleventyConfig.addPassthroughCopy("src/fonts");
   eleventyConfig.addPassthroughCopy("src/javascript");
+  eleventyConfig.addPassthroughCopy("src/robots.txt");
 
   /* Markdown Plugins */
   let markdownIt = require("markdown-it");
